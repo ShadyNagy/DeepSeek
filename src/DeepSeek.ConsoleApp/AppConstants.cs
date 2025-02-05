d57ce9c4c83b072b68f55ce18eea321ecc88f628
ShadyNagy <info@shadynagy.com>
2025-02-06 01:10:31 +0200

    rem.

--- src/DeepSeek.ConsoleApp/AppConstants.cs
+++ src/DeepSeek.ConsoleApp/AppConstants.cs
@@ -1,6 +1,6 @@
 ﻿namespace DeepSeek.ConsoleApp;
 internal static class AppConstants
 {
-  public static string API_KEY = "sk-bf7cb63c24204388aaea08cc48b20995";
+  public static string API_KEY = string.Empty;
   public static string BASE_URL = "https://api.deepseek.com";
 }
